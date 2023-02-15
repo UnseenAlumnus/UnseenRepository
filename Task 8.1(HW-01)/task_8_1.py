d8eed85337b2cb58e3a58e4ae101552d06c5f3da
UnseenAlumnus <anton.smirnov.92@icloud.com>
2023-02-15 21:01:20 +0300

    Task 8.1 files added

--- Task 8.1(HW-01)/task_8_1.py
+++ Task 8.1(HW-01)/task_8_1.py
@@ -9,36 +9,38 @@ def score_game(random_predict) -> int:
         int: среднее количество попыток
     """
     count_ls = []
-    #np.random.seed(1)  # фиксируем сид для воспроизводимости
+    np.random.seed(1)  # фиксируем сид для воспроизводимости
     random_array = np.random.randint(1, 101, size=(10000))  # загадали список чисел
 
     for number in random_array:
         count_ls.append(random_predict(number))
 
     score = int(np.mean(count_ls))
-    print(f"Ваш алгоритм угадывает число в среднем за: {score} попытки")
-def game_core_v2(number: int = 1) -> int:
-    """Сначала устанавливаем любое random число, а потом уменьшаем
-    или увеличиваем его в зависимости от того, больше оно или меньше нужного.
-       Функция принимает загаданное число и возвращает число попыток
-       
+    print(f"Ваш алгоритм угадывает число в среднем за: {score} попыток")
+    
+def game_core_v3(number: int = 1) -> int:
+    """
     Args:
         number (int, optional): Загаданное число. Defaults to 1.
 
     Returns:
         int: Число попыток
     """
+    # Ваш код начинается здесь
     count = 0
     predict = np.random.randint(1, 101)
+    number = 50
     while number != predict:
         count += 1
         if number > predict:
             predict += 2
         elif number < predict:
-            predict -= 2
+            predict -= 1.5
+    # Ваш код заканчивается здесь
 
     return count
-#Run benchmarking to score effectiveness of all algorithms
 
-print('Run benchmarking for game_core_v2: ', end='')
-score_game(game_core_v2)
+print('Run benchmarking for game_core_v3: ', end='')
+#RUN
+if __name__ == '__main__':
+    score_game(game_core_v3)
